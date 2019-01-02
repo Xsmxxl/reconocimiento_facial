33713528c2e2ec6ed21aa970392c51c00120a483
Xsmxxl Uchiha <ismael-uchiha1@hotmail.com>
2019-01-02 02:31:59 -0500

    iniciando pruebas de detección de rostro con camara frontal del pc

--- prueba.py
+++ prueba.py
@@ -1,22 +1,28 @@
-import cv2
+import cv2, os
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-# Read the image
-image = cv2.imread("prueba.jpeg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.2,
-    minNeighbors=5,
-    minSize=(30, 30),
-    #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-    flags = cv2.CASCADE_SCALE_IMAGE
-)
-print("Found {0} faces!".format(len(faces)))
+os.system('cls')
+video_capture = cv2.VideoCapture(0)
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+
+while True:
+    # Read the image
+    otro, image = video_capture.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30),
+        #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    #print("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow("Cara encontrada", image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
